0d292776fd9cd58617205ed64b973a51daad62e4
Peter Kirschner <peter@kirschners.de>
2016-10-20 07:09:02 +0200

    Configuration

--- io.klib.chat.provider/src/io/klib/chat/provider/ChatImpl.java
+++ io.klib.chat.provider/src/io/klib/chat/provider/ChatImpl.java
@@ -4,10 +4,18 @@ import org.osgi.service.component.annotations.Component;
 
 import io.klib.chat.api.Chat;
 import io.klib.chat.api.Message;
+import org.osgi.service.metatype.annotations.ObjectClassDefinition;
+import org.osgi.service.metatype.annotations.Designate;
 
+@ObjectClassDefinition
+@interface Configuration {
+	String user_name() default "osgi";
+}
+
+@Designate(ocd=Configuration.class)
 @Component(
-	name = "io.klib.chat", 
-	property = "user.name=osgi"
+		name = "io.klib.chat", 
+		property = "user.name=osgi"
 )
 public class ChatImpl implements Chat {
 
